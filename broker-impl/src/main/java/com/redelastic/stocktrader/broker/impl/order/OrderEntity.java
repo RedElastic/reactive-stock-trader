0c45f666e1d9aba766e63ca7d237fb94c64310a3
Dana Harrington <dana.harrington@redelastic.com>
2019-01-11 10:13:30 -0500

    Split out behaviour for each state of portfolio into a separate inner class. This allows us to organize the entity logic and almost isolate each state's behaviour (we still need access to the `newBehaviorBuilder`).

--- broker-impl/src/main/java/com/redelastic/stocktrader/broker/impl/order/OrderEntity.java
+++ broker-impl/src/main/java/com/redelastic/stocktrader/broker/impl/order/OrderEntity.java
@@ -3,11 +3,13 @@ package com.redelastic.stocktrader.broker.impl.order;
 import akka.Done;
 import com.lightbend.lagom.javadsl.persistence.PersistentEntity;
 import com.redelastic.stocktrader.broker.api.OrderResult;
+import com.redelastic.stocktrader.broker.api.OrderStatus;
 import com.redelastic.stocktrader.order.Order;
 import com.redelastic.stocktrader.order.OrderDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -21,13 +23,17 @@ public class OrderEntity extends PersistentEntity<OrderCommand, OrderEvent, Opti
                 .flatMap(Function.identity())
                 .map(orderState -> {
                     if (orderState instanceof OrderState.Pending) {
-                        return pendingOrderBehavior(((OrderState.Pending) orderState).getOrderDetails());
+                        return new PendingBehaviorBuilder((OrderState.Pending)orderState).getBehavior();
+                    } else if (orderState instanceof OrderState.Fulfilled) {
+                        return new FulfilledOrderBehaviorBuilder((OrderState.Fulfilled) orderState).getBehavior();
+                    } else if (orderState instanceof OrderState.Failed) {
+                        throw new IllegalStateException(); // FIXME: implement this behavior
                     } else {
                         throw new IllegalStateException();
                     }
-                }).orElse(uninitializedBehaviour());
+                }).orElse(new UninitializedBehaviorBuilder().getBehavior());
     }
-
+/*
     private Behavior uninitializedBehaviour() {
         BehaviorBuilder builder = newBehaviorBuilder(Optional.empty());
         builder.setCommandHandler(OrderCommand.PlaceOrder.class, (cmd,ctx) -> {
@@ -42,6 +48,8 @@ public class OrderEntity extends PersistentEntity<OrderCommand, OrderEvent, Opti
     }
 
     private Behavior pendingOrderBehavior(OrderDetails orderDetails) {
+        return new PendingBehaviorBuilder(orderDetails).getBehavior();
+
         BehaviorBuilder builder = newBehaviorBuilder(
                 Optional.of(new OrderState.Pending(orderDetails)));
 
@@ -64,16 +72,21 @@ public class OrderEntity extends PersistentEntity<OrderCommand, OrderEvent, Opti
         handleGetStatus(builder);
         ignoreRepeats(builder);
         return builder.build();
+
     }
 
+
     private Behavior completedOrderBehavior(OrderState orderState) {
+
+
         BehaviorBuilder builder = newBehaviorBuilder(Optional.of(orderState));
         handleGetStatus(builder);
         ignoreRepeats(builder);
         return builder.build();
     }
 
-    private void handleGetStatus(BehaviorBuilder builder) {
+
+    /*private void handleGetStatus(BehaviorBuilder builder) {
         builder.setReadOnlyCommandHandler(OrderCommand.GetStatus.class, (cmd,ctx) -> {
             if (!state().isPresent()) {
                 ctx.reply(Optional.empty());
@@ -82,10 +95,11 @@ public class OrderEntity extends PersistentEntity<OrderCommand, OrderEvent, Opti
             }
         });
     }
-
+*/
     /* Ignore duplicate submissions of an order, provided the details are the same. Duplicates can happen due to
      * at-least-once handling of the order placement process.
      */
+    /*
     private void ignoreRepeats(BehaviorBuilder builder) {
         builder.setReadOnlyCommandHandler(OrderCommand.PlaceOrder.class, (cmd, ctx) -> {
             OrderDetails orderDetails = cmd.getOrderDetails();
@@ -104,8 +118,149 @@ public class OrderEntity extends PersistentEntity<OrderCommand, OrderEvent, Opti
         });
     }
 
+
     private Order order() {
         return new Order(entityId(), state().get().getOrderDetails());
     }
+    */
+
+    private interface OrderBehaviorBuilder {
+        Behavior getBehavior();
+    }
+
+    private abstract class OrderBehaviourBuilder<State> implements OrderBehaviorBuilder {
+
+        public String entityId() { return OrderEntity.this.entityId(); }
+        public Logger getLogger() { return OrderEntity.this.log; }
+
+        abstract OrderDetails getOrderDetails();
+        abstract OrderStatus getOrderStatus();
+
+        abstract State state(); // shadow parent's state method to provide a more specific one for this behaviour.
+
+        Order getOrder() { return new Order(entityId(), getOrderDetails()); }
+
+        void getStatus(OrderCommand.GetStatus cmd, ReadOnlyCommandContext ctx) {
+            ctx.reply(Optional.of(getOrderStatus()));
+        }
+
+        void ignoreDuplicatePlacements(OrderCommand.PlaceOrder cmd, ReadOnlyCommandContext<Order> ctx) {
+            OrderDetails orderDetails = cmd.getOrderDetails();
+            if (orderDetails.equals(getOrderDetails())) {
+                ctx.reply(getOrder());
+            } else {
+                getLogger().warn(String.format(
+                        "Order %s, existing: %s, received %s",
+                        entityId(),
+                        getOrderDetails(),
+                        orderDetails.toString()
+                ));
+                ctx.commandFailed(new InvalidCommandException(
+                        String.format("Attempt to place different order with same order ID: %s", entityId())));
+            }
+        }
+
+        void setCommonBehavior(BehaviorBuilder builder) {
+            builder.setReadOnlyCommandHandler(OrderCommand.GetStatus.class, this::getStatus);
+            builder.setReadOnlyCommandHandler(OrderCommand.PlaceOrder.class, this::ignoreDuplicatePlacements);
+        }
+    }
+
+
+    private class PendingBehaviorBuilder extends OrderBehaviourBuilder<OrderState.Pending> {
+        private final OrderDetails orderDetails;
+
+        public OrderDetails getOrderDetails() { return orderDetails; }
+        public OrderStatus getOrderStatus() { return OrderStatus.Pending; }
+
+        public OrderState.Pending state() { return new OrderState.Pending(orderDetails); };
+
+        PendingBehaviorBuilder(OrderDetails orderDetails) {
+            this.orderDetails = orderDetails;
+        }
+
+        PendingBehaviorBuilder(OrderState.Pending state) {
+            this(state.getOrderDetails());
+        }
+
+        private Persist complete(OrderCommand.Complete cmd, CommandContext<Done> ctx) {
+            if (cmd.getOrderResult() instanceof OrderResult.OrderCompleted) {
+                OrderResult.OrderCompleted orderCompleted = (OrderResult.OrderCompleted)cmd.getOrderResult();
+                return ctx.thenPersist(new OrderEvent.OrderFulfilled(getOrder(), orderCompleted.getTrade()),
+                        evt -> ctx.reply(Done.getInstance()));
+            } else if (cmd.getOrderResult() instanceof OrderResult.OrderFailed) {
+                return ctx.thenPersist(new OrderEvent.OrderFailed(getOrder()),
+                        evt -> ctx.reply(Done.getInstance()));
+            } else {
+                throw new IllegalStateException();
+            }
+        }
+
+        private Behavior fulfilled(OrderEvent.OrderFulfilled evt) {
+            return new FulfilledOrderBehaviorBuilder(orderDetails, evt.getTrade().getPrice()).getBehavior();
+        }
+
+        public Behavior getBehavior() {
+            BehaviorBuilder builder = newBehaviorBuilder(Optional.of(new OrderState.Pending(this.orderDetails)));
+            setCommonBehavior(builder);
+
+            builder.setCommandHandler(OrderCommand.Complete.class, this::complete);
+            builder.setEventHandlerChangingBehavior(OrderEvent.OrderFulfilled.class, this::fulfilled);
+
+            return builder.build();
+        }
+
+    }
+
+    private class FulfilledOrderBehaviorBuilder extends OrderBehaviourBuilder<OrderState.Fulfilled> {
+        private final OrderDetails orderDetails;
+
+        private final BigDecimal price;
+
+        public OrderDetails getOrderDetails() { return orderDetails; }
+        public OrderStatus getOrderStatus() { return OrderStatus.Pending; }
+        public String entityId() { return OrderEntity.this.entityId(); }
+        public Logger getLogger() { return OrderEntity.this.log; }
+
+        public OrderState.Fulfilled state() { return new OrderState.Fulfilled(orderDetails, this.price); }
+
+        FulfilledOrderBehaviorBuilder(OrderDetails orderDetails, BigDecimal price) {
+            this.orderDetails = orderDetails;
+            this.price = price;
+        }
+
+        FulfilledOrderBehaviorBuilder(OrderState.Fulfilled state) {
+            this(state.getOrderDetails(), state.getPrice());
+        }
+
+        public Behavior getBehavior() {
+            BehaviorBuilder builder = newBehaviorBuilder(Optional.of(new OrderState.Fulfilled(orderDetails, this.price)));
+            setCommonBehavior(builder);
+
+            return builder.build();
+        }
+    }
+
+    private class UninitializedBehaviorBuilder {
+        Behavior getBehavior() {
+            BehaviorBuilder builder = newBehaviorBuilder(Optional.empty());
+            builder.setCommandHandler(OrderCommand.PlaceOrder.class, this::placeOrder);
+
+            builder.setEventHandlerChangingBehavior(OrderEvent.ProcessingOrder.class, this::processing);
+            return builder.build();
+        }
+
+        private Persist placeOrder(OrderCommand.PlaceOrder cmd, CommandContext<Done> ctx) {
+            Order order = new Order(entityId(), cmd.getOrderDetails());
+            return ctx.thenPersist(
+                    new OrderEvent.ProcessingOrder(order),
+                    evt -> ctx.reply(order));
+        }
+
+        private Behavior processing(OrderEvent.ProcessingOrder evt) {
+            return new PendingBehaviorBuilder(evt.getOrder().getDetails()).getBehavior();
+        }
+    }
+
 
 }
