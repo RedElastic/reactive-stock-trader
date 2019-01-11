2b75819c2598fbff788f1ef057cfe295b1eef321
Dana Harrington <dana.harrington@redelastic.com>
2019-01-11 12:32:21 -0500

    Reorganize PortfolioEntity behaviors using inner classes.

--- portfolio-impl/src/main/java/com/redelastic/stocktrader/portfolio/impl/PortfolioEntity.java
+++ portfolio-impl/src/main/java/com/redelastic/stocktrader/portfolio/impl/PortfolioEntity.java
@@ -33,12 +33,6 @@ public class PortfolioEntity extends PersistentEntity<PortfolioCommand, Portfoli
                 .orElse(becomeUninitialized());
     }
 
-    private void rejectOpen(BehaviorBuilder builder) {
-        builder.setCommandHandler(Open.class, (setup, ctx) -> {
-            ctx.commandFailed(new PortfolioAlreadyOpened(entityId()));
-            return ctx.done();
-        });
-    }
 
     private Behavior becomeUninitialized() {
         BehaviorBuilder builder = newBehaviorBuilder(PortfolioState.Uninitialized.INSTANCE);
@@ -68,27 +62,13 @@ public class PortfolioEntity extends PersistentEntity<PortfolioCommand, Portfoli
      * @return
      */
     private Behavior becomeOpened(PortfolioState.Open initialState) {
-        BehaviorBuilder builder = newBehaviorBuilder(initialState);
-        rejectOpen(builder);
-        handlePlaceOrder(builder);
-        handleCompletedTrade(builder);
-        handleFundEvents(builder);
-        handleShareEvents(builder);
-        handleGetState(builder);
-        handleLiquidate(builder);
-        return builder.build();
+        return new OpenPortfolioBehavior(initialState).getBehavior();
     }
 
     private Behavior becomeClosed() {
-        BehaviorBuilder builder = newBehaviorBuilder(PortfolioState.Closed.INSTANCE);
-        rejectOpen(builder);
-        return builder.build();
+        return new ClosedPortfolioBehaviourBuilder().getBehavior();
     }
 
-    private void handleGetState(BehaviorBuilder builder) {
-        builder.setReadOnlyCommandHandler(GetState.class, (cmd, ctx) ->
-                ctx.reply(((PortfolioState.Open)state())));
-    }
 
     private void handleLiquidate(BehaviorBuilder builder) {
         builder.setCommandHandler(PortfolioCommand.Liquidate.class, (cmd, ctx) -> {
@@ -98,85 +78,136 @@ public class PortfolioEntity extends PersistentEntity<PortfolioCommand, Portfoli
         });
     }
 
-    private void handleCompletedTrade(BehaviorBuilder builder) {
-        builder.setCommandHandler(PortfolioCommand.CompleteTrade.class, this::completeTrade);
-    }
 
-    private Persist completeTrade(PortfolioCommand.CompleteTrade cmd, CommandContext<Done> ctx) {
-        Trade trade = cmd.getTrade();
-        log.warn(String.format(
-                "Portfolio %s processing trade %s",
-                entityId(),
-                trade.toString()
-        ));
-        switch(trade.getOrderType()) {
-            case BUY:
-                return ctx.thenPersistAll(Arrays.asList(
-                        new PortfolioEvent.FundsDebited(entityId(), trade.getPrice()),
-                        new PortfolioEvent.SharesCredited(entityId(), trade.getSymbol(), trade.getShares())),
-                        () -> ctx.reply(Done.getInstance()));
 
-            case SELL:
-                // Note: for a sale the shares have already been removed when we initiated the sale
-                return ctx.thenPersistAll(Arrays.asList(
-                        new PortfolioEvent.FundsCredited(entityId(), trade.getPrice())),
-                        () -> ctx.reply(Done.getInstance()));
+    abstract class PortfolioBehaviorBuilder<State extends PortfolioState> {
+        State state() { return (State)PortfolioEntity.this.state(); }
 
-            default:
-                throw new IllegalStateException(); // FIXME
+        abstract Behavior getBehavior();
+
+        void rejectOpen(BehaviorBuilder builder) {
+            builder.setCommandHandler(Open.class, (setup, ctx) -> {
+                ctx.commandFailed(new PortfolioAlreadyOpened(entityId()));
+                return ctx.done();
+            });
         }
     }
 
-    private void handleFundEvents(BehaviorBuilder builder) {
-        builder.setEventHandler(PortfolioEvent.FundsDebited.class, evt -> ((PortfolioState.Open)state()).update(evt));
-        builder.setEventHandler(PortfolioEvent.FundsCredited.class, evt -> ((PortfolioState.Open)state()).update(evt));
-    }
+    private class OpenPortfolioBehavior extends PortfolioBehaviorBuilder<PortfolioState.Open> {
+        private final Behavior behavior;
 
+        OpenPortfolioBehavior(PortfolioState.Open state) {
+            BehaviorBuilder builder = newBehaviorBuilder(state);
+            rejectOpen(builder);
+            handlePlaceOrder(builder);
+            handleCompletedTrade(builder);
+            handleFundEvents(builder);
+            handleShareEvents(builder);
+            handleGetState(builder);
+            handleLiquidate(builder);
+            this.behavior = builder.build();
+        }
 
-    private void handleShareEvents(BehaviorBuilder builder) {
-        builder.setEventHandler(PortfolioEvent.SharesCredited.class, evt -> ((PortfolioState.Open)state()).update(evt));
-        builder.setEventHandler(PortfolioEvent.SharesDebited.class, evt -> ((PortfolioState.Open)state()).update(evt));
-    }
+        private void handleCompletedTrade(BehaviorBuilder builder) {
+            builder.setCommandHandler(PortfolioCommand.CompleteTrade.class, this::completeTrade);
+        }
 
-    private void handlePlaceOrder(BehaviorBuilder builder) {
-        builder.setCommandHandler(PortfolioCommand.PlaceOrder.class, this::placeOrder);
-
-        builder.setEventHandler(PortfolioEvent.OrderPlaced.class, evt -> {
-            log.warn("Portfolio entity got OrderPlaced event.");
-            return ((PortfolioState.Open)state()).update(evt);
-
-        });
-    }
-
-    private Persist placeOrder(PortfolioCommand.PlaceOrder placeOrder, CommandContext<Done> ctx) {
-        log.warn(String.format("Placing order %s", placeOrder.getOrder().toString()));
-        Order order = placeOrder.getOrder();
-        OrderDetails orderDetails = placeOrder.getOrder().getDetails();
-        PortfolioState.Open state = (PortfolioState.Open)state();
-        switch(orderDetails.getOrderType()) {
-            case SELL:
-                int available = state.getHoldings().getShareCount(orderDetails.getSymbol());
-                if (available >= orderDetails.getShares()) {
+        private Persist completeTrade(PortfolioCommand.CompleteTrade cmd, CommandContext<Done> ctx) {
+            Trade trade = cmd.getTrade();
+            log.warn(String.format(
+                    "Portfolio %s processing trade %s",
+                    entityId(),
+                    trade.toString()
+            ));
+            switch(trade.getOrderType()) {
+                case BUY:
                     return ctx.thenPersistAll(Arrays.asList(
-                            new PortfolioEvent.OrderPlaced(entityId(), order),
-                            new PortfolioEvent.SharesDebited(entityId(), orderDetails.getSymbol(), orderDetails.getShares())),
+                            new PortfolioEvent.FundsDebited(entityId(), trade.getPrice()),
+                            new PortfolioEvent.SharesCredited(entityId(), trade.getSymbol(), trade.getShares())),
                             () -> ctx.reply(Done.getInstance()));
-                } else {
-                    ctx.commandFailed(new InsufficientShares(
-                            String.format("Insufficient shares of %s for sell, %d required, %d held.",
-                                    orderDetails.getSymbol(),
-                                    orderDetails.getShares(),
-                                    available)));
-                    return ctx.done();
-                }
-            case BUY:
-                return ctx.thenPersist(new PortfolioEvent.OrderPlaced(entityId(), order),
-                        evt -> ctx.reply(Done.getInstance()));
-            default:
-                throw new IllegalStateException();
+
+                case SELL:
+                    // Note: for a sale the shares have already been removed when we initiated the sale
+                    return ctx.thenPersistAll(Arrays.asList(
+                            new PortfolioEvent.FundsCredited(entityId(), trade.getPrice())),
+                            () -> ctx.reply(Done.getInstance()));
+
+                default:
+                    throw new IllegalStateException(); // FIXME
+            }
         }
+
+        private void handleFundEvents(BehaviorBuilder builder) {
+            builder.setEventHandler(PortfolioEvent.FundsDebited.class, evt -> state().update(evt));
+            builder.setEventHandler(PortfolioEvent.FundsCredited.class, evt -> state().update(evt));
+        }
+
+        private void handlePlaceOrder(BehaviorBuilder builder) {
+            builder.setCommandHandler(PortfolioCommand.PlaceOrder.class, this::placeOrder);
+
+            builder.setEventHandler(PortfolioEvent.OrderPlaced.class, evt -> {
+                log.warn("Portfolio entity got OrderPlaced event.");
+                return ((PortfolioState.Open)state()).update(evt);
+
+            });
+        }
+
+        private Persist placeOrder(PortfolioCommand.PlaceOrder placeOrder, CommandContext<Done> ctx) {
+            log.warn(String.format("Placing order %s", placeOrder.getOrder().toString()));
+            Order order = placeOrder.getOrder();
+            OrderDetails orderDetails = placeOrder.getOrder().getDetails();
+            PortfolioState.Open state = (PortfolioState.Open)state();
+            switch(orderDetails.getOrderType()) {
+                case SELL:
+                    int available = state.getHoldings().getShareCount(orderDetails.getSymbol());
+                    if (available >= orderDetails.getShares()) {
+                        return ctx.thenPersistAll(Arrays.asList(
+                                new PortfolioEvent.OrderPlaced(entityId(), order),
+                                new PortfolioEvent.SharesDebited(entityId(), orderDetails.getSymbol(), orderDetails.getShares())),
+                                () -> ctx.reply(Done.getInstance()));
+                    } else {
+                        ctx.commandFailed(new InsufficientShares(
+                                String.format("Insufficient shares of %s for sell, %d required, %d held.",
+                                        orderDetails.getSymbol(),
+                                        orderDetails.getShares(),
+                                        available)));
+                        return ctx.done();
+                    }
+                case BUY:
+                    return ctx.thenPersist(new PortfolioEvent.OrderPlaced(entityId(), order),
+                            evt -> ctx.reply(Done.getInstance()));
+                default:
+                    throw new IllegalStateException();
+            }
+        }
+
+        private void handleShareEvents(BehaviorBuilder builder) {
+            builder.setEventHandler(PortfolioEvent.SharesCredited.class, evt -> state().update(evt));
+            builder.setEventHandler(PortfolioEvent.SharesDebited.class, evt -> state().update(evt));
+        }
+
+        private void handleGetState(BehaviorBuilder builder) {
+            builder.setReadOnlyCommandHandler(GetState.class, (cmd, ctx) ->
+                    ctx.reply(state()));
+        }
+
+        Behavior getBehavior() { return this.behavior; }
     }
 
+    private class ClosedPortfolioBehaviourBuilder extends PortfolioBehaviorBuilder<PortfolioState.Closed> {
+        private final Behavior behavior;
 
+        ClosedPortfolioBehaviourBuilder() {
+            BehaviorBuilder builder = newBehaviorBuilder(PortfolioState.Closed.INSTANCE);
+            rejectOpen(builder);
+
+            this.behavior = builder.build();
+        }
+
+        @Override
+        Behavior getBehavior() {
+            return behavior;
+        }
+    }
 
 }
