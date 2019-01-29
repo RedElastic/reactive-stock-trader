36aa394cdfe76d5726af009ec07e2cf54dbef782
Dana Harrington <dana.harrington@redelastic.com>
2019-01-29 12:50:38 -0500

    Split out TransferRepository interface for DI

--- wire-transfer-impl/src/main/java/com/redelastic/stockbroker/wireTransfer/impl/transfer/TransferRepositoryImpl.java
+++ wire-transfer-impl/src/main/java/com/redelastic/stockbroker/wireTransfer/impl/transfer/TransferRepositoryImpl.java
@@ -11,20 +11,22 @@ import com.redelastic.stocktrader.TransferId;
 
 import javax.inject.Inject;
 
-public class TransferRepository {
+public class TransferRepositoryImpl implements TransferRepository {
 
     private final PersistentEntityRegistry entityRegistry;
 
     @Inject
-    public TransferRepository(PersistentEntityRegistry entityRegistry) {
+    public TransferRepositoryImpl(PersistentEntityRegistry entityRegistry) {
         entityRegistry.register(TransferEntity.class);
         this.entityRegistry = entityRegistry;
     }
 
+    @Override
     public PersistentEntityRef<TransferCommand> get(TransferId transferId) {
         return entityRegistry.refFor(TransferEntity.class, transferId.getId());
     }
 
+    @Override
     public Source<Pair<TransferEvent, Offset>, NotUsed> eventStream(AggregateEventTag<TransferEvent> tag, Offset offset) {
         return entityRegistry.eventStream(tag, offset);
     }
