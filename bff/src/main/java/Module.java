48b138125a207436b82c18a10eeadc09249053b9
Kevin Webber <kevin.webber@gmail.com>
2019-07-01 12:45:26 -0400

    working on kubernetes deployment scripts

--- bff/src/main/java/Module.java
+++ bff/src/main/java/Module.java
@@ -7,6 +7,10 @@ import com.google.inject.AbstractModule;
 import com.lightbend.lagom.javadsl.api.ServiceAcl;
 import com.lightbend.lagom.javadsl.api.ServiceInfo;
 import com.lightbend.lagom.javadsl.client.ServiceClientGuiceSupport;
+import com.lightbend.lagom.javadsl.client.ConfigurationServiceLocator;
+import com.lightbend.lagom.javadsl.api.ServiceLocator;
+import play.Environment;
+import com.typesafe.config.Config;
 import com.redelastic.stocktrader.broker.api.BrokerService;
 import com.redelastic.stocktrader.portfolio.api.PortfolioService;
 import com.redelastic.stocktrader.wiretransfer.api.WireTransferService;
@@ -15,6 +19,14 @@ import services.quote.QuoteServiceImpl;
 
 @SuppressWarnings("WeakerAccess")
 public class Module extends AbstractModule implements ServiceClientGuiceSupport {
+    private final Environment environment;
+    private final Config config;
+
+    public Module(Environment environment, Config config) {
+        this.environment = environment;
+        this.config = config;
+    }
+
     @Override
     protected void configure() {
         // route all paths to through this Play BFF
